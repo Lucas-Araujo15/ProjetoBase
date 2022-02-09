8e371eea01913895aca17d2ec730c0e42f6bb900
Lucas Araujo Oliveira Silva <37140363854@info.scs>
2022-02-09 11:06:21 -0300

    Criptografa as senhas dos usuários

--- Back-end/Patrimonio/Repositories/UsuarioRepository.cs
+++ Back-end/Patrimonio/Repositories/UsuarioRepository.cs
@@ -1,6 +1,7 @@
 ﻿using Patrimonio.Contexts;
 using Patrimonio.Domains;
 using Patrimonio.Interfaces;
+using Patrimonio.Utils;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -20,7 +21,59 @@ namespace Patrimonio.Repositories
 
         public Usuario Login(string email, string senha)
         {
-            return ctx.Usuarios.FirstOrDefault(u => u.Email == email && u.Senha == senha);
+            //var usuarioEncontrado = ctx.Usuarios.FirstOrDefault(u => u.Email == email);
+
+            //if (usuarioEncontrado != null)
+            //{
+            //    bool comparado = Criptografia.Comparar(senha, usuarioEncontrado.Senha);
+            //    if (comparado)
+            //    {
+            //        return usuarioEncontrado;
+            //    }
+            //    else
+            //    {
+            //        usuarioEncontrado.Senha = Criptografia.GerarHash(usuarioEncontrado.Senha);
+            //        comparado = Criptografia.Comparar(senha, usuarioEncontrado.Senha);
+            //        if (comparado)
+            //        {
+            //            ctx.Usuarios.Update(usuarioEncontrado);
+            //            ctx.SaveChanges();
+            //            return usuarioEncontrado;
+            //        }
+            //        else
+            //        {
+            //            return null;
+            //        }
+            //    }
+            //}
+
+            //return null;
+
+            Usuario usuarioEncontrado = ctx.Usuarios.FirstOrDefault(u => u.Email == email && u.Senha == senha);
+
+            if (usuarioEncontrado != null)
+            {
+                usuarioEncontrado.Senha = Criptografia.GerarHash(usuarioEncontrado.Senha);
+                ctx.Usuarios.Update(usuarioEncontrado);
+                ctx.SaveChanges();
+
+                return usuarioEncontrado;
+            }
+
+            else
+            {
+                usuarioEncontrado = ctx.Usuarios.FirstOrDefault(u => u.Email == email);
+
+                if (usuarioEncontrado != null)
+                {
+                    if (Criptografia.Comparar(senha, usuarioEncontrado.Senha))
+                    {
+                        return usuarioEncontrado;
+                    }
+                }
+            }
+
+            return null;
         }
     }
 }
